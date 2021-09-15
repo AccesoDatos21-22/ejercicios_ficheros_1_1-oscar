09b8b62356c5620411f1707c9c9d8fac45aa1624
oscar <trissaikyt.g@gmail.com>
2021-09-15 14:25:25 +0200

    readme

--- src/org/madrid/ad/ut01/ficheros/Ejercicios.java
+++ src/org/madrid/ad/ut01/ficheros/Ejercicios.java
@@ -3,7 +3,6 @@ package org.madrid.ad.ut01.ficheros;
 import java.io.IOException;
 
 import java.io.File;
-import java.io.IOException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
